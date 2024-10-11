11657c57f8ba8a0659c3b1b73056367a92f8246d
trankieuvy <trankieuvy2005@gmail.com>
2024-10-11 11:06:23 +0700

    B31tamgiac.py

--- LAB01C5/B31tamgiac.py
+++ LAB01C5/B31tamgiac.py
@@ -8,16 +8,15 @@ Created on Sat Sep 14 02:00:21 2024
 a=int(input("Nhập vào số thứ nhất: "))
 b=int(input("Nhập vào số thứ hai: "))
 c=int(input("Nhập vào số thứ ba: "))
-while a<0 and b<0 and c<0:
-    print("Lỗi,ba số không phải là số nguyên dương")
+if a+b>0 or a+c>0 or b+c>0:
+     print(f"{a},{b},{c} là 3 cạnh của tam giác")
+if a==b==c:
+    print("Tam giác đều")
+elif a==b or a==c or b==c:
+    print("Tam giác cân")
+elif a**2+b**2==c**2 or a**2+c**2==b**2 or b**2+c**2==a**2:
+    print("Tam giác vuông")
 else:
-    if a==b==c:
-        print("Tam giác đều")
-    elif a==b or a==c or b==c:
-        print("Tam giác cân")
-    elif a**2+b**2==c**2 or a**2+c**2==b**2 or b**2+c**2==a**2:
-        print("Tam giác vuông")
-    else:
-        print("Tam giác thường")
+    print("Tam giác thường")
         
     
