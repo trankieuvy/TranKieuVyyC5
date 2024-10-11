1a561292d7c68b3bfd668fc6d0cdd1b90354e199
trankieuvy <trankieuvy2005@gmail.com>
2024-10-11 11:15:11 +0700

    B46.py

--- LAB01C5/B46.py
+++ LAB01C5/B46.py
@@ -5,9 +5,17 @@ Created on Sat Sep 14 20:36:47 2024
 @author: trankieuvy
 """
 
-for z in range(1, 979 // 9 + 1):  
-    for y in range(1, (979 - 9 * z) // 7 + 1):
-        x =(979-7*y-9*z)//2
-    if x>0 and x.is_integer():
-        print("Nghiệm của phương trình là: ","x= ",int(x),"y=",y,"z=",z)
+danhsach=[]
+
+for x in range (1,490):
+    for y in range (1,140):
+        for z in range (1,109):
+            if 2*x + 7*y + 9*z == 979:
+                danhsach+=[(x,y,z)]
+for i in danhsach:
+    print("Bộ nghiệm",i)
+   
+#Kiểm tra điều kiện của x,y,z
+while x<=0 and y<=0 and z<=0:
+    print("không hợp lệ")
         
