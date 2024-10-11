d8a096240147de561b5afc46697d0c53eef503b0
trankieuvy <trankieuvy2005@gmail.com>
2024-10-11 11:11:14 +0700

    B33chinhphuong.py

--- LAB01C5/B33chinhphuong.py
+++ LAB01C5/B33chinhphuong.py
@@ -5,18 +5,14 @@ Created on Sat Sep 14 11:37:18 2024
 @author: trankieuvy23717511
 
 """
-
+import math
 n=int(input("Nhập vào số nguyên dương n: "))
-if n<0:
-    print("Số âm không thể là số chính phương")
+while n<= 0:
+    n= int(input("Nhập lại n"))
+if math.sqrt(n) == int(math.sqrt(n)):
+    print("n là số chính phương")
 else:
-    i=0
-    while i*i<=n:
-        if i*i==n:
-            print(n,"là số chính phương")
-            break
-        i+=1
-    else:
-        print(n,"không phải là số chính phương")
-        
-        
+    print("n không phải là số chính phương")
+    
+    
+
